7c4f11d1ea5c333ea82f736d9ff7d5a6d3f69d89
Nick Ames <names144@gmail.com>
2015-02-05 21:03:36 -0800

    Added more physics to the player. Also added bigger sandbox map and static evil bunnies.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ window.onload = function() {
     	// Load the spritesheet for our player, indicating the location, size of sprites, and the number of images on the sheet
 		game.load.spritesheet('player', 'images/bmario.png', 32, 32, 18);
 
+		// Load the spritesheet for the ground bunnies
+		game.load.spritesheet('evilGroundBunny', 'images/evil_bunny_transparent.png', 32, 32, 6);
+
 		// Loads the tilemap data from the JSON exported from tiled.
 		game.load.tilemap('tilemap', 'json/prototype-map.json', null, Phaser.Tilemap.TILED_JSON);
 		
@@ -23,6 +26,11 @@ window.onload = function() {
     }
 
     var player;		// The Player of the game
+
+    // Ground bunnies
+	var groundBunnies = [];
+	var groundBunnyLocations = [];
+
 	var map;		// The game world map
 	var bg;			// The background image of the game
 	var layer;		// The layer that holds the tilemap
@@ -61,17 +69,39 @@ window.onload = function() {
   		layer.resizeWorld();
 
   		// Create the player and position in the world with given name
-	    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+	    player = game.add.sprite(64, 64, 'player');
+
+	    player.attributes = new Player();
+
+	    // Prepare the bunny locations
+	    groundBunnyLocations[0] = {x: 700, y: 336};
+	    groundBunnyLocations[1] = {x: 1200, y: 592};
+	    groundBunnyLocations[2] = {x: 829, y: 848};
+	    groundBunnyLocations[3] = {x: 369, y: 976};
+	    groundBunnyLocations[4] = {x: 1130, y: 1168};
+	    groundBunnyLocations[5] = {x: 1800, y: 1392};
+	    groundBunnyLocations[6] = {x: 2546, y: 1616};
+	    groundBunnyLocations[7] = {x: 2500, y: 1936};
+	    groundBunnyLocations[8] = {x: 1720, y: 2128};
+	    groundBunnyLocations[9] = {x: 1060, y: 2640};
+
+	    // Create the ground bunnies and position them
+	    for (var i = 0; i < 10; i++) {
+	    	groundBunnies[i] = game.add.sprite(groundBunnyLocations[i].x, groundBunnyLocations[i].y, 'evilGroundBunny');
+	    	game.physics.enable(groundBunnies[i], Phaser.Physics.ARCADE);
+	    	groundBunnies[i].attributes = new EvilGroundBunny();
+	    	groundBunnies[i].attributes.create(groundBunnies[i], game);
+	    }
 
 	    // Enable physics on the player
     	game.physics.enable(player, Phaser.Physics.ARCADE);
 
     	// Creates the player
-	    createPlayer(player, game);
+	    player.attributes.create(player, game);
 	    
 	    // Set the camera to follow the player
 	    game.camera.follow(player);
-		
+
 	}
 
 	/*
@@ -82,8 +112,19 @@ window.onload = function() {
 		// Tell the physics engine to collide between the player and our layer for the world
 	    game.physics.arcade.collide(player, layer);
 
+	    // Collision with the players and the bunnies
+	    game.physics.arcade.collide(player, groundBunnies, player.attributes.collide);
+
+	    // Set the collision for the bunnies and the world
+	    game.physics.arcade.collide(groundBunnies, layer);
+
 	    // Updates for the player
-	    updatePlayer(player, game);
+	    player.attributes.update(player, game);
+
+	    // Updates for the ground bunnies
+	    for (var i = 0; i < 10; i++) {
+	    	groundBunnies[i].attributes.update(groundBunnies[i], game);
+	    }
 	}
 
 	/*
